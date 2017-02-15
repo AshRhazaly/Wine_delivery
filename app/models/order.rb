0b5f90047dabe336ce05948148900d278275ca26
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-15 22:27:08 +0800

    Added validation checks for form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :wine
+  validates :cust_name, :email, :address,
+            :quantity, presence: true
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,3 +1,5 @@
 class Wine < ApplicationRecord
   has_many :orders, :dependent => :destroy
+  validates :name, :description, :year, :price,
+            presence: true
 end

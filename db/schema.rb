b3fea01c12966411cc838a460eb80278a4fdd97e
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-15 16:54:54 +0800

    Add price column as decimal.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215081358) do
+ActiveRecord::Schema.define(version: 20170215085409) do
 
   create_table "wines", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170215081358) do
     t.string   "year"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.decimal  "price"
   end
 
 end

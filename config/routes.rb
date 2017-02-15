8c8fca30b2eec1ed9d680ca28604afd85e555986
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-15 16:18:14 +0800

    Generate a controller for wines.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'wines/new'
+
+  get 'wines/create'
+
+  get 'wines/edit'
+
+  get 'wines/update'
+
+  get 'wines/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

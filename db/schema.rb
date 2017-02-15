79d5a44b6dd31456e0d7f89728c9277b6c295cbe
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-15 18:43:59 +0800

    Add price column to master branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215101202) do
+ActiveRecord::Schema.define(version: 20170215104320) do
 
   create_table "custorders", force: :cascade do |t|
     t.string   "cust_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170215101202) do
     t.string   "year"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.decimal  "price"
   end
 
 end

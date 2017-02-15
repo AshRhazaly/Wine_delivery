04477e170a0e28efda76c7e2ca70513c82552f5f
icebear <wt@wtcom.local>
2017-02-15 18:55:02 +0800

    Another bs commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215104320) do
+ActiveRecord::Schema.define(version: 20170215101202) do
 
   create_table "custorders", force: :cascade do |t|
     t.string   "cust_name"

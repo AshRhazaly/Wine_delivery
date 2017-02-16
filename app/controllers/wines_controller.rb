312461bb6c74542148e55d7da436734d46e137d4
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-16 13:56:40 +0800

    Add exceptions to authentication

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,6 +1,6 @@
 class WinesController < ApplicationController
   before_action :set_wine, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show, :search]
   def index
     @wine = Wine.all
   end

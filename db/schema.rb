abdf7903ddba715fb1dfccd7fba40ba003e44453
icebear <wt@wtcom.local>
2017-02-16 16:17:28 +0800

    Migrate after users login implemented

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170216054358) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean  "admin"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"

3861a3024ef43c222ba2c6d9406028ddf701bfc2
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-19 01:02:50 +0800

    Add name to order model (renamed cust_name to name)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218170133) do
+ActiveRecord::Schema.define(version: 20170218170224) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170218170133) do
     t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|

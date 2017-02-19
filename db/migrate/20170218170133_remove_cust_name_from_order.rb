744165a1bfeca46dc42c8bcf927f11933f82b400
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-19 18:11:10 +0800

    Delete migration files

--- db/migrate/20170218170133_remove_cust_name_from_order.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCustNameFromOrder < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :orders, :cust_name, :string
-  end
-end
--- db/migrate/20170218170224_add_name_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToOrder < ActiveRecord::Migration[5.0]
-  def change
-    add_column :orders, :name, :string
-  end
-end
